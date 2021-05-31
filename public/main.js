10fb015ce5a2eea967bc065cf91b77f211eb1ea0
Prateek Diwedi <diwedi1112@gmail.com>
2021-05-31 13:49:37 -0700

    auto updater

--- public/main.js
+++ public/main.js
@@ -1,9 +1,11 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 
 const path = require("path");
 const isDev = require("electron-is-dev");
 
 require("@electron/remote/main").initialize();
+
+const { autoUpdater } = require("electron-updater");
 
 function createWindow() {
   // Create the browser window.
@@ -21,6 +23,10 @@ function createWindow() {
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
+
+  win.once("ready-to-show", () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
 }
 
 app.on("ready", createWindow);
@@ -38,4 +44,19 @@ app.on("activate", function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
+});
+
+autoUpdater.on("update-available", () => {
+  win.webContents.send("update_available");
+});
+autoUpdater.on("update-downloaded", () => {
+  win.webContents.send("update_downloaded");
+});
+
+ipcMain.on("restart_app", () => {
+  autoUpdater.quitAndInstall();
 });
